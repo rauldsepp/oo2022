826a1d4b3ddd7278b7050dfaebe290b17e1894bc
Raul Raudsepp <rauldsepp@users.noreply.github.com>
2022-02-07 15:13:59 +0200

    Funktsioonid2

---
+++ 01_funktsioon/Funktsioonid2.java
@@ -0,0 +1,22 @@
+public class Funktsioonid2{
+	public static double sentimeetriteks(double tollid){
+		return tollid*2.54;
+	}
+	public static double tollideks(double sentimeetrid){
+		return sentimeetrid/2.54;
+	}
+	public static double[] reaalarvudeks(String[] sd){
+		double[] v=new double[sd.length];
+		for(int i=0; i<sd.length; i++){
+			v[i]=Double.parseDouble(sd[i]);
+		}
+		return v;
+	}
+	//Tehke funktsiooniga sentimeetrid tollideks
+	public static void main(String[] args){
+		double[] arvud=reaalarvudeks(args);
+		for(int i=0;i<arvud.length; i++){
+			System.out.println(sentimeetriteks(arvud[i]));
+		}
+	}
+}
